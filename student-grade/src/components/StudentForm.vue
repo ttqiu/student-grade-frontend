5fd87ef1187fa4b18a07fa43be42fc01d42b69a2
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-03-16 16:37:52 -0400

    studentForm

---
+++ student-grade/src/components/StudentForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="form-container">
+    <form v-on:submit="handleSubmit">
+      <input
+        @input="handleChange"
+        placeholder="First Name"
+        :value="firstName"
+        name="firstName"
+        type="firstName"
+      />
+      <input
+        @input="handleChange"
+        placeholder="Last Name"
+        :value="lastName"
+        name="lastName"
+        type="lastName"
+      />
+      <input
+        @input="handleChange"
+        placeholder="Email"
+        :value="email"
+        name="email"
+        type="email"
+      />
+      <button :disabled="!email || !firstName || !lastName">Add Student</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from axios
+
+export default {
+  name: 'StudentForm',
+  data: () => ({
+    firstName: '',
+    lastName: '',
+    email: ''
+  }),
+  methods: {
+    handleChange(e) {
+      this[e.target.name] = e.target.value
+    },
+    async handleSubmit(e) {
+      e.preventDefault()
+      await axios.post(
+        `http://localhost:3001/api/register`, { firstName: this.firstName, lastName: this.lastName,  email: this.email,}
+      )
+      this.firstName = ''
+      this.lastName = ''
+      this.email = ''
+    }
+  }
+}
+</script>
