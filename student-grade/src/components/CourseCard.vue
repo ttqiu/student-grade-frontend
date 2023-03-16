80fd42b39b06848d9f5e44b887cd6ac85b85f21a
odonovan1 <odogdonovan@gmail.com>
2023-03-16 18:19:55 -0400

    prepull foramt

--- student-grade/src/components/CourseCard.vue
+++ student-grade/src/components/CourseCard.vue
@@ -3,10 +3,7 @@
     <div class="info-wrapper flex-col">
       <h3>{{ course.name }}</h3>
       <h3>{{ course.department }}</h3>
-<<<<<<< HEAD
-      <!-- <h3>{{ course.Grade.grade }}</h3> -->
-=======
->>>>>>> c1a8d19d7f9292ac9457a8f2a0defd919bfdfb22
+
     </div>
   </div>
 </template>
