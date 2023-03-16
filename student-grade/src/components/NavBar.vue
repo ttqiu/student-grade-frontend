8b0756084bcf9b92f140ced6d34d59dd7c0ea52e
Hyun Bin Yim <hby0818@gmail.com>
2023-03-16 15:46:21 -0400

    navbar

---
+++ student-grade/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/courses">Courses</router-link>
+    <router-link to="/students">Students</router-link>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar'
+  }
+</script>
+
+<style scoped>
+</style>
