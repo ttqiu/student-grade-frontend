588ac7f4e44d79f654ebf0e919eb76153301d869
Hyun Bin Yim <hby0818@gmail.com>
2023-03-16 16:27:30 -0400

    routerjs

---
+++ student-grade/src/router.js
@@ -0,0 +1,25 @@
+import { createWebHistory, createRouter } from "vue-router"
+import HomePage from './pages/HomePage'
+import ViewCourses from './pages/ViewCourses'
+import ViewStudents from './pages/ViewStudents'
+import CourseDetails from './pages/CourseDetails'
+import StudentDetails from './pages/StudentDetails'
+import CourseForm from './components/CourseForm'
+import StudentForm from './components/StudentForm'
+
+const routes = [
+  { path: '/', component: HomePage, name: 'HomePage' },
+  { path: '/courses', component: ViewCourses, name: 'ViewCourses' },
+  { path: '/students', component: ViewStudents, name: 'ViewStudents' },
+  { path: '/courses/:course_id', component: CourseDetails, name: 'CourseDetails' },
+  { path: '/students/:student_id', component: StudentDetails, name: 'StudentDetails' },
+  { path: '/courses/add', component:CourseForm, name: 'CourseForm' },
+  { path: '/students/add', component: StudentForm, name: 'StudentForm' },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
