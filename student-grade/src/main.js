00c054768b9a2768e677dac5219d6a9762ffc326
odonovan1 <odogdonovan@gmail.com>
2023-03-16 18:50:32 -0400

    added basic styling

--- student-grade/src/main.js
+++ student-grade/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 
-// import './styles/App.css'
+import './App.css'
 
 createApp(App).use(router).mount('#app')
