2a27b1f41f7d87b5d75d655c965feda437ce32de
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-03-16 15:48:15 -0400

    courseCard

---
+++ student-grade/src/components/CourseCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <div class="info-wrapper flex-col">
+      <h3>{{ course.name }}</h3>
+      <h3>{{ course.department }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CourseCard',
+  props: {
+    course: {}
+  }
+}
+</script>
