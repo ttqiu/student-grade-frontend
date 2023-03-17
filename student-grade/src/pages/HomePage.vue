a63fd8e52856420cde35224de07be9d94058c24f
Hyun Bin Yim <hby0818@gmail.com>
2023-03-17 12:44:20 -0400

    morecss

--- student-grade/src/pages/HomePage.vue
+++ student-grade/src/pages/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="background">
-    <img class="logoimage" src="../images/hedge.png" alt="hedgehoglogo" />
+    <img class="logoimage" src="../images/hedgecloudsee.png" alt="hedgehoglogo" />
     <div class="view">
       <h3 @click="viewStudents()" class="h3-button">View Students</h3>
       <h3 @click="viewCourses()" class="h3-button">View Courses</h3>
